88f775c00ee2b1db52413c867a5be734388d9937
Akib <imranulhassan3296@gmail.com>
2025-01-23 22:26:24 +0600

    Update

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -2,7 +2,7 @@
 const page = () => {
     return (
         <div className="w-72 mx-auto mt-14">
-            <p className="text-2xl ">Welcome to Your Profile</p>
+            <p className="text-2xl ">Welcome to Your Profile!</p>
         </div>
     );
 };
--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -2,7 +2,7 @@
 
 const BlogDetails = ({blog}) => {
     return (
-        <div className="container pl-10 mx-auto py-8">
+        <div className="w-9/12 pl-12 mt-8 mx-auto py-8">
           <h1 className="text-3xl font-bold mb-4">{blog.title}</h1>
           <p className="text-gray-700">{blog.body}</p>
         </div>

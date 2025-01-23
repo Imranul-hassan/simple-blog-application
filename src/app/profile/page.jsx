67586a1223cf4b02028858bbb262600a82841205
Akib <imranulhassan3296@gmail.com>
2025-01-23 22:37:17 +0600

    Update design

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,8 +1,8 @@
 
 const page = () => {
     return (
-        <div className="w-72 mx-auto mt-14">
-            <p className="text-2xl ">Welcome to Your Profile!</p>
+        <div className="w-96 mx-auto mt-10">
+            <p className="text-3xl ">Welcome to Your Profile!</p>
         </div>
     );
 };
